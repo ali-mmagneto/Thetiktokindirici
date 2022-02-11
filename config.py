9590cb6084591d8105a6ed6ff87f9041a5979385
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-02-11 23:59:02 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
   
-    APP_ID = int(os.environ.get("ADMIN_ID", 12345))
+    ADMIN_ID = int(os.environ.get("ADMIN_ID", 12345))
