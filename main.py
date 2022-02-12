15a175b0122a5b3a4e922579610d9051b8941d06
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-02-12 11:45:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -184,7 +184,7 @@ async def text(message: types.Message):
                     chat_id=message.chat.id,
                     video=file.read(),
                     caption='@TikTokVideoDownRobot ile indirildi.', 
-                    reply_markup=InlineKeyboardMarkup( [ [InlineKeyboardButton('Destek Ol Yada Olma', url=f"https://t.me/mmagneto3") ] ]  ) 
+                    reply_markup=InlineKeyboardMarkup( [ [InlineKeyboardButton('Bot', url=f"https://t.me/TikTokVideoDownRobot") ] ]  ) 
                 )
             await bot.delete_message(message.chat.id, message.message_id + 1)
             os.remove(path)
